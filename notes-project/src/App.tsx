4baea1d6f3136f527f3ad459d3c8854038806dc1
NathanAtaliba <nathanataliba28@gmail.com>
2024-04-22 16:55:48 -0300

    add frontend

--- notes-project/src/App.tsx
+++ notes-project/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="bg-slate-500 w-full h-screen justify-center content-center">
+      <form className="flex flex-col bg-slate-900 md:w-1/4 md:h-1/2 text-white text-center justify-center content-center rounded-md box-border font-bold text-base items-center">
+        <label>Email:</label>
+        <input className="bg-transparent text-center border-2 border-solid border-slate-500 rounded-md outline-green-400 p-2 w-1/2 placeholder:text-slate-300 m-1" type="text" placeholder="Enter with yor email"/>
+        <label>Password:</label>
+        <input className="bg-transparent text-center border-2 border-solid border-slate-500 rounded-md outline-green-400 p-2 w-1/2 placeholder:text-slate-300 m-1" type="text" placeholder="Enter with yor password"/>
+        <div className="flex flex-row">
+          <button className="bg-green-500 border-solid border-2 text-center p-3 m-2 rounded-md ">Login</button>
+          <button className="bg-slate-500 border-solid border-2 text-center p-3 m-2 rounded-md ">Create Login</button>
+        </div>
+        <a href="" className="text-red-400">Esqueceu a senha ?</a>
+      </form>
+    </div>
+  </>
+  );
 }
-
-export default App
